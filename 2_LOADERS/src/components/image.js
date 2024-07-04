ac7372d3e78660df4d2b4193422fae090295a9cf
fernandoap46 <fernandoap46@gmail.com>
2024-07-04 15:12:25 -0300

    incluindo uma segunda imagem utilizando o loader

--- 2_LOADERS/src/components/image.js
+++ 2_LOADERS/src/components/image.js
@@ -1,9 +1,18 @@
 import Space from '../img/space.jpg'
+import Space2 from '../img/space2.jpg'
 class Image{
     insertSpaceImage(){
         const img = document.createElement('img');
 
         img.src = Space;
+        img.width=200;
+
+        document.querySelector('body').appendChild(img);
+    }
+    insertSpaceImage2(){
+        const img = document.createElement('img');
+
+        img.src = Space2;
         img.width=200;
 
         document.querySelector('body').appendChild(img);
